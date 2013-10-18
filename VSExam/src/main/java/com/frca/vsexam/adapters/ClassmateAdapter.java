43afba52a5a4a6e930ac8c0299b2ac8f7751bebb
Frca <killerfrca@gmail.com>
2013-10-19 00:13:48 +0200

    Fix sending too much requests
    Signed-off-by: Frca <killerfrca@gmail.com>

--- VSExam/src/main/java/com/frca/vsexam/adapters/ClassmateAdapter.java
+++ VSExam/src/main/java/com/frca/vsexam/adapters/ClassmateAdapter.java
@@ -1,6 +1,8 @@
 package com.frca.vsexam.adapters;
 
 import android.content.Context;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,8 @@ import com.frca.vsexam.entities.ExamList;
 import com.frca.vsexam.helper.Helper;
 import com.frca.vsexam.helper.ImageDownloaderTask;
 
+import java.util.List;
+
 /**
  * Created by KillerFrca on 5.10.13.
  */
@@ -22,6 +26,7 @@ public class ClassmateAdapter extends ArrayAdapter<String> {
 
     private ClassmateList classmates;
 
+    private SparseArray<ImageDownloaderTask> tasks = new SparseArray<ImageDownloaderTask>();
     private static final int resourceLayout = R.layout.classmates_item;
 
     public ClassmateAdapter(Context context, ClassmateList classmates) {
@@ -38,6 +43,7 @@ public class ClassmateAdapter extends ArrayAdapter<String> {
     public View getView(final int position, final View convertView, final ViewGroup parent) {
         Classmate classmate = classmates.get(position);
 
+
         View view;
         if (convertView != null)
             view = convertView;
@@ -51,8 +57,12 @@ public class ClassmateAdapter extends ArrayAdapter<String> {
         TextView text2 = (TextView)view.findViewById(R.id.text2);       // date
         TextView text3 = (TextView)view.findViewById(R.id.text3);       // time
 
+        if (tasks.get(classmate.id) == null) {
+            ImageDownloaderTask task = new ImageDownloaderTask(getContext(), logo);
+            task.loadLogo(classmate.id);
+            tasks.put(classmate.id, task);
+        }
 
-        new ImageDownloaderTask(getContext(), logo).loadLogo(classmate.id);
         text1.setText(classmate.name);
         text2.setText(Helper.getDateOutput(classmate.registered, Helper.DateOutputType.DATE));
         text3.setText(Helper.getDateOutput(classmate.registered, Helper.DateOutputType.TIME));
