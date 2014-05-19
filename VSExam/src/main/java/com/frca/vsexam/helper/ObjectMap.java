8afb30839ccb9b0d8b5d26b10fd7c57016eed2aa
Frca <killerfrca@gmail.com>
2014-05-19 23:12:46 +0200

    Fix Object map equals method
    Signed-off-by: Frca <killerfrca@gmail.com>

--- VSExam/src/main/java/com/frca/vsexam/helper/ObjectMap.java
+++ VSExam/src/main/java/com/frca/vsexam/helper/ObjectMap.java
@@ -1,5 +1,7 @@
 package com.frca.vsexam.helper;
 
+import android.util.Log;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,12 +10,10 @@ public class ObjectMap extends HashMap<String, Object> {
     @Override
     public boolean equals(Object o) {
         if (o instanceof ObjectMap) {
-            boolean equals = true;
             ObjectMap otherMap = (ObjectMap)o;
-            for (Map.Entry entry : entrySet()) {
-                if (entry.getValue() != otherMap.get(entry.getKey()))
+            for (Map.Entry<String, Object> entry : entrySet())
+                if (!entry.getValue().equals(otherMap.get(entry.getKey())))
                     return false;
-            }
 
             return true;
         }
@@ -21,10 +21,9 @@ public class ObjectMap extends HashMap<String, Object> {
     }
 
     public boolean isInList(List<ObjectMap> list) {
-        for (ObjectMap map : list) {
+        for (ObjectMap map : list)
             if (equals(map))
                 return true;
-        }
 
         return false;
     }
